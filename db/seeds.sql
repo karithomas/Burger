21b5cb7a0a124a04c8e6cff73af05df0e1eef86e
karithomas <kari.k.thomas8@gmail.com>
2020-01-29 09:04:59 -0500

    updating files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES("cheese burger", TRUE);
+INSERT INTO burgers(burger_name, devoured) VALUES("chicken burger", TRUE);
+INSERT INTO burgers(burger_name, devoured) VALUES("ginormous burger", TRUE);
---
+++ models/burger.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(result) {
+            cb(result);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(result) {
+            cb(result);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(result) {
+            cb(result);
+        });
+    }
+};
+module.exports = burger;
+  
